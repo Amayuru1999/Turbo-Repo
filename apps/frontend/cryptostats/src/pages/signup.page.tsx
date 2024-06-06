b8ef47ba3a8a6683f3c011a167501525931eaca6
Amayuru1999 <amarasingheau@gmail.com>
2024-06-06 16:41:42 +0530

    Issues Fixed

--- apps/frontend/cryptostats/src/pages/signup.page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { LoginForm } from "../components/auth/login-form/login-form.component";
-
-
-const SignupPage: React.FC=()=>{
-    return<LoginForm/>
-}
-
-export{SignupPage};
